63aab2266536091f7c477b413415086a444609eb
Ismaël Lajaaiti <ismael.lajaaiti@gmail.com>
2024-08-02 11:35:33 +0200

    Update SI figure on short vs overall resp.

--- scripts/SI_predictability_reactivity-resilience.jl
+++ scripts/SI_predictability_reactivity-resilience.jl
@@ -7,6 +7,7 @@ using QuadGK
 using Random
 using SpeciesReactivity
 using Statistics
+using StatsBase
 
 Random.seed!(111) # For reproduciblity.
 
@@ -27,12 +28,13 @@ create_df() = DataFrame(;
     overall_response = Float64[],
     predictability = Float64[],
 )
-n_perturbations = 1_000
+n_perturbations = 100
 intensity = 0.6
 df = create_df()
 A = com.A
 yield = equilibrium_abundance(com)
 reactivity = [get_reactivity(com.A, yield, i) for i in 1:S]
+
 for k in 1:n_perturbations
     @info "Perturbation $k"
     x0 = proportional_perturbation(yield, intensity * sqrt(S), 1, true)
@@ -86,8 +88,8 @@ with_theme(publication_theme) do
     width = two_third_page_width * cm_to_pt
     height = width / width_height_ratio
     save_figure(
-        "figures/SI_predictability",
-        # "/tmp/plot",
+        # "figures/SI_predictability",
+        "/tmp/plot",
         fig,
         (width, height),
     )
@@ -98,19 +100,20 @@ eta = LinRange(0.0, 0.5, 100)
 with_theme(publication_theme) do
     fig = Figure()
     panel_a = fig[1, 1] = GridLayout()
-    panel_b = fig[2, 1] = GridLayout()
-    panel_c = fig[3, 1] = GridLayout()
+    panel_b = fig[1, 2] = GridLayout()
+    panel_c = fig[1, 3] = GridLayout()
     markersize = 8
     strokewidth = 0.5
     color = :grey
-    ax1 = Axis(
-        fig[1, 1];
-        xlabel = "Species relative yield",
-        ylabel = "Species response intensity",
+    ax2 = Axis(
+        fig[1, 2];
+        xlabel = "Species average \n initial response",
+        # ylabel = "Species overall response intensity",
         yscale = log10,
+        # xscale = log10,
     )
     errorbars!(
-        processed_df.yield,
+        -processed_df.yield,
         processed_df.overall_response,
         processed_df.overall_response_error;
         linewidth = 1,
@@ -118,17 +121,18 @@ with_theme(publication_theme) do
         color = :black,
     )
     scatter!(
-        processed_df.yield,
+        -processed_df.yield,
         processed_df.overall_response;
         color,
         markersize,
         strokewidth,
     )
-    ax2 = Axis(
-        fig[2, 1];
+    ax1 = Axis(
+        fig[1, 1];
         xlabel = "Species reactivity",
-        ylabel = "Species response intensity",
+        ylabel = "Species overall \n response intensity",
         yscale = log10,
+        # xscale = log10,
     )
     errorbars!(
         processed_df.reactivity,
@@ -146,22 +150,25 @@ with_theme(publication_theme) do
         strokewidth,
     )
     ax3 = Axis(
-        fig[3, 1];
-        xlabel = "Species reactivity over \n relative yield",
-        ylabel = "Species response intensity",
+        fig[1, 3];
+        xlabel = "Inverse of \n species resistance",
+        # ylabel = "Species response intensity",
         yscale = log10,
-        xscale = log10,
+        # xscale = log10,
     )
     ratio = processed_df.reactivity ./ processed_df.yield
+    f(x) = x / (1 + x)
     errorbars!(
-        ratio,
+        f.(ratio),
         processed_df.overall_response,
         processed_df.overall_response_error;
         linewidth = 1,
         whiskerwidth = 3,
         color = :black,
     )
-    scatter!(ratio, processed_df.overall_response; color, markersize, strokewidth)
+    scatter!(f.(ratio), processed_df.overall_response; color, markersize, strokewidth)
+    hideydecorations!(ax2)
+    hideydecorations!(ax3)
     for (layout, label) in zip([panel_a, panel_b, panel_c], letters)
         Label(
             layout[1, 1, TopLeft()],
@@ -172,12 +179,12 @@ with_theme(publication_theme) do
         )
     end
     isdir("figures") || mkdir("figures")
-    width = two_third_page_width * cm_to_pt
-    height = width * 3 / width_height_ratio
+    width = full_page_width * cm_to_pt
+    height = width * 0.6 / width_height_ratio
     save_figure(
         "figures/SI_reactivity-resilience",
         # "/tmp/plot",
         fig,
-        (width, height),
+        1.3 .* (width, height),
     )
 end
